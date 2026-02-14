cde17a8e23adc796b97af5e40f9bda28b055775d
lijinsong2025 <jinsong@isrc.iscas.ac.cn>
2026-02-14 19:04:46 +0800

    tests: add PyInteger allocation/refactor unit tests

---
+++ src/runtime/PyInteger_refactor_tests.cpp
@@ -0,0 +1,28 @@
+#include "gtest/gtest.h"
+
+#include "runtime/PyInteger.hpp"
+
+using namespace py;
+
+TEST(PyIntegerRefactor, CreateReturnsCorrectValue)
+{
+    auto r = PyInteger::create(12345);
+    ASSERT_TRUE(r.is_ok());
+    auto *i = r.unwrap();
+    ASSERT_NE(i, nullptr);
+    EXPECT_EQ(i->as_i64(), 12345);
+}
+
+#ifdef PYLANG_USE_SHARED_PTR
+TEST(PyIntegerRefactor, RegistryWithSharedPtrMode)
+{
+    auto r = PyInteger::create(42);
+    ASSERT_TRUE(r.is_ok());
+    auto *raw = r.unwrap();
+
+    auto sp = py::compat::try_get_shared_for_legacy(raw);
+    // If the type is constructible in this context, compat layer should register
+    EXPECT_NE(sp, nullptr);
+    if (sp) { EXPECT_EQ(sp.get(), raw); }
+}
+#endif
