1a11567b4e8028190b203e4079f0cddb2cfd45d2
lijinsong2025 <jinsong@isrc.iscas.ac.cn>
2026-04-04 21:03:03 +0800

    chore: remove choro files

--- fix_pymodule_erase.py
+++
@@ -1,7 +0,0 @@
-with open('src/runtime/PyModule.cpp', 'r') as f:
-    text = f.read()
-
-text = text.replace('m_dict->map().erase(it);', 'm_dict->remove(attribute);')
-
-with open('src/runtime/PyModule.cpp', 'w') as f:
-    f.write(text)
--- fix_rt_attr.py
+++
@@ -1,8 +0,0 @@
-with open('src/runtime/export/rt_attr.cpp', 'r') as f:
-    text = f.read()
-
-text = text.replace('shape->lookup(name)', 'shape->lookup(py::PyString::intern(name))')
-
-with open('src/runtime/export/rt_attr.cpp', 'w') as f:
-    f.write(text)
-
